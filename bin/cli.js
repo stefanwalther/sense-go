6fb47a2baca5e65bea06ea4b4b99ebf9ea812d90
Stefan Walther <Stefan.Walther@qlik.com>
2016-07-11 01:24:04 +0200

    Cli processing

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('sense-go');
+'use strict';
+var gulp = require( 'gulp' );
+var argv = require('minimist')(process.argv.slice(2));
+var senseGo = require( './../lib/' );
+
+senseGo.init( gulp, function () {
+	
+	gulp.series( argv._)();
+
+} );
--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var gulp = require('gulp');
-var senseGo = require('./lib/');
-
-senseGo.init( gulp, function (  ) {});
