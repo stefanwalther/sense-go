d4fc5b6bf5c55a97b6e359490a7ff9239d6d920e
Stefan Walther <Stefan.Walther@qlik.com>
2016-07-15 23:35:34 +0200

    tests: Added tests for htmlmin

---
+++ test/html-min.spec.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// core dependencies
+var path = require( 'path' );
+var fs = require( 'fs-extra' );
+
+// local dependencies
+var senseGo = require( './../lib/' );
+var chai = require( 'chai' );
+var chaiFiles = require( 'chai-files' );
+chai.use( chaiFiles );
+var file = chaiFiles.file;
+var expect = chai.expect;
+var testUtils = require( './lib/test-utils' );
+var htmlMinTask = require('./../lib/tasks/htmlmin');
+
+describe( 'HtmlMin tasks', function () {
+
+	var tmpDir = path.join( __dirname, './.tmp' );
+
+	it( 'load properly', function ( done ) {
+		senseGo.init( function () {
+			expect( senseGo.tasks ).to.have.property( 'htmlmin:tmp' );
+			done();
+		} );
+	} );
+
+	xit( 'transforms properly', function ( done ) {
+		var gulp = senseGo.gulp;
+
+		var globalConfig = {
+			htmlmin: {
+				preserveComments: true
+			}
+		};
+		var config = {
+			taskName: 'htmlmin',
+			src: path.join(__dirname, './fixtures/html-min/**/*.htm[l]'),
+			dest: path.join(__dirname, './.tmp')
+		};
+		var plugins = require('./../lib/pluginLoader');
+		var taskUtils = require('./../lib/taskUtils')( plugins, config );
+		htmlMinTask( gulp, plugins, globalConfig, taskUtils).htmlmin( config, function() {
+			done();
+		} );
+	} );
+
+} );
