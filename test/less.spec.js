28e2717a46f27ec6c8616304231835f36115e46d
Stefan Walther <Stefan.Walther@qlik.com>
2016-07-15 19:00:51 +0200

    test(less): Simplify beforeEach tasks

--- test/less.spec.js
+++ test/less.spec.js
@@ -14,22 +14,23 @@ var testUtils = require( './lib/test-utils' );
 
 describe( 'less tasks (with custom configuration)', function () {
 
+	var tmpDir = path.join( __dirname, './.tmp' );
+
+	beforeEach( function ( done ) {
+		testUtils.delDir( tmpDir, done );
+	} );
+	afterEach( function ( done ) {
+		testUtils.delDir( tmpDir, done );
+	} );
+
 	it( 'should run lessEach', function ( done ) {
 
 		var config = {
 			lessEach: {
 				src: path.join( __dirname, './fixtures/lessEach/**/*.less' ),
-				dest: path.join( __dirname, './.tmp' )
-			},
-			tmpDir: path.join( __dirname, './.tmp' )
+				dest: tmpDir
+			}
 		};
-
-		before( function ( done ) {
-			testUtils.delDir( config.tmpDir, done );
-		} );
-		after( function ( done ) {
-			testUtils.delDir( config.tmpDir, done );
-		} );
 
 		senseGo.init( config, function ( err ) {
 			expect( err ).to.be.undefined;
@@ -49,17 +50,9 @@ describe( 'less tasks (with custom configuration)', function () {
 		var config = {
 			lessReduce: {
 				src: path.join( __dirname, './fixtures/lessReduce/root.less'),
-				dest: path.join( __dirname, './.tmp' )
-			},
-			tmpDir: path.join( __dirname, './.tmp')
+				dest: tmpDir
+			}
 		};
-
-		before( function ( done ) {
-			testUtils.delDir( config.tmpDir, done );
-		} );
-		after( function ( done ) {
-			testUtils.delDir( config.tmpDir, done );
-		} );
 
 		senseGo.init( config, function ( err ) {
 			expect( err ).to.be.undefined;
