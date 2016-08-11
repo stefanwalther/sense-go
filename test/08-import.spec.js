fce0b90089fdfb4709e332ff000f62e2b2e843ec
Stefan Walther <Stefan.Walther@qlik.com>
2016-08-11 16:29:58 +0200

    test(import): Add bare minimum tasks to check if the import:fromLocal task is loaded

--- test/08-import.spec.js
+++ test/08-import.spec.js
@@ -1,1 +1,35 @@
 'use strict';
+'use strict';
+
+// core dependencies
+var path = require( 'path' );
+var fs = require( 'fs-extra' );
+
+// local dependencies
+var senseGo = require( './../lib/' );
+var chai = require( 'chai' );
+var chaiFiles = require( 'chai-files' );
+chai.use( chaiFiles );
+var file = chaiFiles.file;
+var expect = chai.expect;
+var testUtils = require( './lib/test-utils' );
+
+describe( 'Import tasks', function () {
+
+	var tmpDir = path.join( __dirname, './.tmp' );
+
+	beforeEach( function ( done ) {
+		testUtils.delDir( tmpDir, done );
+	} );
+	afterEach( function ( done ) {
+		testUtils.delDir( tmpDir, done );
+	} );
+
+	it( 'load properly', function ( done ) {
+		senseGo.init( function () {
+			expect( senseGo.tasks ).to.have.property( 'import:fromLocal' );
+			done();
+		} );
+	} );
+
+} );
