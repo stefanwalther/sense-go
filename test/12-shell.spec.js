fe7de47bfac7f2477d874899a8de70a9b433cf63
Stefan Walther <swr@qlik.com>
2017-11-10 23:49:27 +0100

    test: catch errors

--- test/12-shell.spec.js
+++ test/12-shell.spec.js
@@ -41,6 +41,7 @@ describe('Shell Tasks', () => {
       expect(senseGo.gulp._registry._tasks).to.have.a.property('shell');
 
       senseGo.gulp.series('shell')((err, result) => {
+        expect(err).to.be.undefined;
         expect(result).to.be.an('array');
         expect(result[0][0]).to.be.equal('1\n');
         expect(result[0][1]).to.be.equal('2\n');
@@ -71,6 +72,7 @@ describe('Shell Tasks', () => {
       expect(senseGo.gulp._registry._tasks).to.have.a.property('shell');
 
       senseGo.gulp.series('shell')((err, result) => {
+        expect(err).to.be.undefined;
         expect(result).to.be.an('array');
         expect(result[0]).to.have.length(1);
         expect(result[0][0]).to.be.equal('2\n');
@@ -99,6 +101,7 @@ describe('Shell Tasks', () => {
       expect(senseGo.gulp._registry._tasks).to.have.a.property('shell');
 
       senseGo.gulp.series('shell')((err, result) => {
+        expect(err).to.be.undefined;
         expect(result).to.be.an('array');
         expect(result[0]).to.have.length(2);
         done();
@@ -123,7 +126,7 @@ describe('Shell Tasks', () => {
       expect(senseGo.gulp._registry._tasks).to.have.a.property('shell');
 
       senseGo.gulp.series('shell')((err, result) => {
-
+        expect(err).to.be.undefined;
         expect(result).to.be.an('array').to.be.eql([undefined]);
         done();
       });
